412dc172a7a0a62cbe3be65b3b17b28a676b5dc7
kibuchi muchai <kibuchimuchai@gmail.com>
2022-03-24 17:16:44 +0300

    Initial commit

---
+++ src/test/java/TicTacToeTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicTacToeTest {
+
+    @Test
+    void checkWinner() {
+    }
+
+    @Test
+    void printBoard() {
+    }
+
+    @Test
+    void main() {
+    }
+}
